2f6454fc775ace6223730f46ca408a1fddffe981
Maria Zamora <Zamora.Mariac@gmail.com>
2015-09-30 16:40:31 -0500

    desktop size

--- dist/contactform.js
+++ dist/contactform.js
@@ -1,6 +1,30 @@
+$(document).on('ready', start);
+
+function start(e){
+
+
+
+}
+
+
+
+
+
+
+
+
 // // $.val()
 // $.html()
 // $.on()
 // $.css()
 // $.show()
 // $.hide()
+
+
+// ERRORS 
+// Name cannot be left empty
+// Email cannot be left empty
+// Email must contain an '@'
+// Website cannot be left empty
+// Website must start with http://
+// Message cannot be left empty
