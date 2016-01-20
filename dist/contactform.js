dd2c9009a62ad7e27369af950bd0dfb5df47939e
Maria Zamora <Zamora.Mariac@gmail.com>
2016-01-20 14:43:04 -0600

    validation start

--- dist/contactform.js
+++ dist/contactform.js
@@ -1,10 +1,56 @@
-$(document).on('ready', start);
+$(document).ready(start);
 
 function start(e){
 
+	$("#form-box").on("submit", function(e){
+		e.preventDefault();
+	});
 
+	var $name = $("#nameinput");
+	var $form = $("#form-box");
+	var $email = $("#emailinput");
+	var $web = $("#webinput");
+
+	$form.on("submit", validate);
+
+
+	function validate(e){
+		var foundError = true;
+
+
+		if($name.val() === ""){
+			foundError = true;
+			$("#name").siblings("#name-error").addClass("active-error").html("Name cannot be left empty");	
+		}
+
+		if($email.val() === ""){
+			foundError = true;
+			$("#email").siblings("#email-error").addClass("active-error").html("Email cannot be left empty");
+		}
+
+		if($web.val() === ""){
+			foundError = true;
+			$("#web").siblings("#web-error").addClass("active-error").html("Website cannot be left empty");
+		}
+		if($email.val() === "@"){
+			foundError = true;
+			$("#email").siblings("#email-error").addClass("active-error").html("Email must contain @");
+		}
+
+	}
 
 }
+
+	// 	if($email.val() === ""){
+	// 		foundError = true;
+	// 		$("email").siblings("#email-error").addClass("active-error").html("Email cannot be left empty");
+	// 	}
+
+	// 	if ($borderLine.val() === ""){
+	// 		foundError = false;
+	// 		$("input").css("border-left", "solid 5px red");
+	// 	}
+	// }
 
 
 
